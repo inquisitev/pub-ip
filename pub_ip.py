59a7049eaf415f6971b8eac3a7df7790bb84297b
Trevor Keegan <tkgn15@live.com>
2025-03-01 12:13:24 -0600

    last fix?

--- pub_ip.py
+++ pub_ip.py
@@ -2,7 +2,7 @@ import subprocess
 import requests
 
 TEAM_NAME = "default"
-with open("TEAM_NAME", "r+") as team_name_file:
+with open(os.path.join("/","etc", "wifinotify", "TEAM_NAME"), "r+") as team_name_file:
     TEAM_NAME = team_name_file.read().strip()
 
 webhook_url = "https://discord.com/api/webhooks/1345273501199896597/5BQjVDqQQQOQr-LWtPEpjnF2-XyxyRW6kfRhGRK5RG3TdfPEaIS3MZ6FcLausGVpvsaL"
